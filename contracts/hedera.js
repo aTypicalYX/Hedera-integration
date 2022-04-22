1775896b51a6d6f27ca47c0f20a4abbcf1a207e8
aTypicalYX <dooyanxu@gmail.com>
2022-04-22 22:46:00 +0800

    commit on master

---
+++ contracts/hedera.js
@@ -0,0 +1,5 @@
+const { session } = await ApiSession.default();
+const helloWorldContract = await Contract.newFrom({ path: './hello_world.sol' });
+const liveContract = await session.upload(helloWorldContract);
+
+console.log(await liveContract.greet());
